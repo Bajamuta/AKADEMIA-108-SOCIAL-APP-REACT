6eb4469ffccdbba8919c116def215b0647fe59f1
jprajzendanc <j.prajzendanc@gmail.com>
2022-12-12 12:04:43 +0100

    forms- first draft

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, {FormEvent, useState} from "react";
 
 export default function Login() {
-    return <div>TEST LOGIN</div>;
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(username, password);
+    }
+
+    return <div className="LoginFormContainer">
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <label form={username}>Username*:</label>
+            <input type="text"
+                   placeholder="Enter username"
+                   value={username} onChange={(e) => setUserName(e.target.value)}/>
+            <label form={password}>Password*:</label>
+            <input type="password" placeholder="Enter password"
+                   value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <button type="submit">Login</button>
+        </form>
+    </div>;
 }
--- src/SignUp.tsx
+++ src/SignUp.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, {FormEvent, useState} from "react";
 
 export default function SignUp() {
-    return <div>SIGNUP TEST</div>;
+
+    const [username, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(username, email, password);
+    }
+
+    return <div className="RegisterFormContainer">
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <label form={username}>Username*:</label>
+            <input type="text"
+                   placeholder="Enter username"
+                   value={username} onChange={(e) => setUserName(e.target.value)}/>
+            <label form={email}>E-mail*:</label>
+            <input type="email" placeholder="Enter e-mail"
+                   value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <label form={password}>Password*:</label>
+            <input type="password" placeholder="Enter password"
+                   value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <label form={passwordConfirm}>Confirm Password*:</label>
+            <input type="password" placeholder="Enter password to confirm"
+                   value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)}/>
+            <button type="submit">Sign Up</button>
+        </form>
+    </div>;
 }

e119e3361c284f8d2d9d5f014565dec3683e7af1
jprajzendanc <j.prajzendanc@gmail.com>
2022-12-12 12:08:56 +0100

    homepage first draft

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default function Home() {
-    return <div>
-        TEST HOME
+    return <div className="PostsContainer">
+        <div className="SinglePost">
+            <div className="SinglePostHeader">
+                <img src="" alt="user image"/>
+                <h3>username</h3>
+                <span>2022-12-01</span>
+            </div>
+            <div className="SinglePostBody">
+                <p>Lorem Ipsum dolor si amet...</p>
+            </div>
+            <div className="SinglePostFooter">
+                <span>12345</span>
+            </div>
+        </div>
     </div>
 }

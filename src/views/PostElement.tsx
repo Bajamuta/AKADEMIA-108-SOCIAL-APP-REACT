c96b8c95deab8ebb104d1cc0e434e3a9416c5c5f
Joanna Prajzendanc <j.prajzendanc@gmail.com>
2022-12-21 00:19:11 +0100

    working on deleting post

--- src/views/PostElement.tsx
+++ src/views/PostElement.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
-import {Post} from "../interfaces";
+import {ObjectContext, Post} from "../interfaces";
 import './PostElement.css';
+import {useOutletContext} from "react-router-dom";
+import axios, {AxiosResponse} from "axios";
+import {API_URL} from "../index";
 
 interface PostProps {
     post: Post
@@ -8,7 +11,31 @@ interface PostProps {
 
 export default function PostElement(props: PostProps) {
 
-    const [likesCount, setLikesCount] = useState(props.post.likes.length);
+    const obj: ObjectContext = useOutletContext();
+
+    const datePipe = (dataString: any) => {
+        const d: Date = new Date(dataString);
+        const year = d.getFullYear();
+        const month = d.getMonth() < 10 ? '0' + d.getMonth() : d.getMonth();
+        const day = d.getDate() < 10 ? '0' + d.getDate() : d.getDate();
+        return year + '-' + month + '-' + day;
+    }
+
+    const [likesCount, setLikesCount] = useState<number>(props.post.likes.length);
+    const [dateOfPost, setDateOfPost] = useState<string>(datePipe(props.post.created_at));
+
+    const deletePost = (id: number) => {
+        axios.post(`${API_URL}/post/delete`, {
+            post_id: id
+        }).then(
+            (response: AxiosResponse<any>) => {
+                console.log(response);
+            }
+        )
+            .catch((error) => {
+                console.error('An error has occurred during deleting the post:', error);
+            })
+    }
 
     return (
         <div className="PostsContainer" key={props.post.id}>
@@ -16,13 +43,17 @@ export default function PostElement(props: PostProps) {
                 <div className="SinglePostHeader">
                     <img src={props.post.user?.avatar_url} alt="user image"/>
                     <h3>{props.post.user?.username}</h3>
-                    <span>created at: {props.post.created_at.toString()}</span>
+                    <span>created at: {dateOfPost}</span>
                 </div>
                 <div className="SinglePostBody">
                     <p key={props.post.id}>{props.post.content}</p>
                 </div>
                 <div className="SinglePostFooter">
                     <span>Likes: {likesCount}</span>
+                    {
+                        obj.loggedUser.username === props.post.user?.username &&
+                        <button className="SinglePostDeleteButton" onClick={() => deletePost(props.post.id)}>Delete</button>
+                    }
                 </div>
             </div>
         </div>

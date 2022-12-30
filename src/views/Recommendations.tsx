f83cfc16e735112aee7edcb1df0703e76c6b88ec
Joanna Prajzendanc <j.prajzendanc@gmail.com>
2022-12-30 02:26:42 +0100

    recommendations init

---
+++ src/views/Recommendations.tsx
@@ -0,0 +1,14 @@
+import React, {useEffect} from "react";
+import "./Recommendations.css";
+
+export default function Recommendations() {
+    useEffect(() => {
+        // getLatestPosts();
+    }, []);
+
+    return (
+        <div className="RecommendationsContainer">
+
+        </div>
+    );
+}
